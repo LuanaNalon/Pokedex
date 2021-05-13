bf619a2af19ee32fd4eeaeb61881c51e0a52d340
Luana Nalon <luananalon@hotmail.com>
2021-05-13 17:11:52 +0100

    Design app and show data

---
+++ Pokedex/Model/Pokemon.swift
@@ -0,0 +1,23 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Luana Nalon on 13/05/2021.
+//
+
+import Foundation
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+
+    var name: String {
+        return _name
+    }
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
---
+++ Pokedex/View/PokeCell.swift
@@ -0,0 +1,29 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Luana Nalon on 13/05/2021.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+}
